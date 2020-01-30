8cf6da420065ed10ed6986d02d5eba3a0a573e3e
AlexBerezkin <mail.berezkin@gmail.com>
2020-01-29 20:45:01 -0800

    Updates

---
+++ precache-manifest.3f9a2edae98fb4330e0fd919b0227676.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4460de3bea9da018397ca10bc8b90fcf",
+    "url": "/zebra/index.html"
+  },
+  {
+    "revision": "ec5f2e072f28146b270a",
+    "url": "/zebra/static/css/main.45fad046.chunk.css"
+  },
+  {
+    "revision": "f94185cb99d72cdc270d",
+    "url": "/zebra/static/js/2.4ad4a7a8.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/zebra/static/js/2.4ad4a7a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec5f2e072f28146b270a",
+    "url": "/zebra/static/js/main.5d847011.chunk.js"
+  },
+  {
+    "revision": "4e8b6902667238a42298",
+    "url": "/zebra/static/js/runtime-main.eec1c4a9.js"
+  },
+  {
+    "revision": "c57294b6dd762511b0f422b21c401e0f",
+    "url": "/zebra/static/media/zebra-logo.c57294b6.svg"
+  }
+]);
